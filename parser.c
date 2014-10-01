26a8aa66fa36308434b4c6d92421fd1d14e69365
dimassantos <dimassantos.ti@gmail.com>
2014-10-01 11:32:20 -0300

    Atualização do parser.c e do parser.h

--- parser.c
+++ parser.c
@@ -1,15 +1,24 @@
+/**
+* Aluno: Dimas Francisco dos Santos - Matricula: 201210796.8
+* Data: 30/9/2014
+***************************************************************************************************
+* Arquivo:  "parser.c"
+* Dependências: "auto_symbol_coment.c" "auto_cahr.c" "scanner.c" "auto_ident_pChave.c" "auto_int_float.c"
+*               "scanner.h" "parser.h"
+***************************************************************************************************
+* Descrição: Contém a todas as funções do parser
+*/
 #include "parser.h"
 
 
-int parser(FILE *inArquivo)
+void parser(FILE *inArquivo)
 {
     TOKEN token;
     p_chamaScanner(inArquivo, &token);
     p_programa(inArquivo, &token);
-    return 1;
 }
 
-/** Verifica a estrutura da funcação main */
+/** PROGRAMA */
 void p_programa(FILE *inArquivo, TOKEN *token)
 {
     if((*token).classificacao == CLASSE_KW_INT )
@@ -30,28 +39,24 @@ void p_programa(FILE *inArquivo, TOKEN *token)
                         p_erroMsg((*token), "o token esperado era EOF");
                     }
                 }
-                else
-                {
+                else{
                     p_erroMsg((*token), "o token esperado era )");
                 }
             }
-            else
-            {
+            else{
                 p_erroMsg((*token), "o token esperado era (");
             }
         }
-        else
-        {
+        else{
             p_erroMsg((*token), "a funcao main deve ser declarada apos int");
         }
     }
-    else
-    {
+    else{
         p_erroMsg((*token), "a funcao main deve precedida de int");
     }
-}
+}//FIM DE PROGRAMA
 
-/** Verifica a estrutura de um <bloco> := “{“ {<decl_var>}* {<comando>}* “}” */
+/** BLOCO */
 void p_bloco(FILE *inArquivo, TOKEN *token)
 {
     if((*token).classificacao == CLASSE_ABRI_CHAVE)
@@ -60,30 +65,29 @@ void p_bloco(FILE *inArquivo, TOKEN *token)
         if((*token).classificacao == CLASSE_KW_INT || (*token).classificacao == CLASSE_KW_FLOAT ||
                 (*token).classificacao ==  CLASSE_KW_CHAR )
         {
-               p_declVar(inArquivo, token);
+               while(p_declVar(inArquivo, token));
         }
 
-        p_comando(inArquivo, token);
+        while(p_comando(inArquivo, token));
 
         if((*token).classificacao == CLASSE_FECHA_CHAVES)
         {
             p_chamaScanner(inArquivo, token);
-            printf("Funciona...\n"); //teste
         }
-        else
-        {
+        else{
             p_erroMsg((*token), "o token esperado era }");
         }
     }
-    else
-    {
+    else{
         p_erroMsg((*token), "o token esperado era {");
     }
-}
-/** Verifica a estrutura de declaração de uma ou mais variaveis */
-void p_declVar(FILE *inArquivo, TOKEN *token)
+}//FIM DE BLOCO
+
+/** DECL_VAR */
+int p_declVar(FILE *inArquivo, TOKEN *token)
 {
-    while((*token).classificacao == CLASSE_KW_INT || (*token).classificacao == CLASSE_KW_FLOAT ||
+    //printf("\nChama p_declVar -  lexema %s   -  Classificacao %i\n",(*token).lexema, (*token).classificacao );
+    if((*token).classificacao == CLASSE_KW_INT || (*token).classificacao == CLASSE_KW_FLOAT ||
                 (*token).classificacao ==  CLASSE_KW_CHAR )
     {
         p_chamaScanner(inArquivo, token);
@@ -96,19 +100,18 @@ void p_declVar(FILE *inArquivo, TOKEN *token)
                 if((*token).classificacao == CLASSE_IDENTIFICADOR)
                 {
                     p_chamaScanner(inArquivo, token);
+
                 }
                 else if((*token).classificacao == CLASSE_PONTO_VIRGULA)
                 {
                     break;
                 }
-                else
-                {
+                else{
                     p_erroMsg((*token), "o token esperado era um identificador");
                 }
             }
         }
-        else
-        {
+        else{
             p_erroMsg((*token), "o token esperado era um identificador");
         }
 
@@ -117,35 +120,33 @@ void p_declVar(FILE *inArquivo, TOKEN *token)
             p_erroMsg((*token), "o token esperado era ;");
         }
         p_chamaScanner(inArquivo, token);
+        return (1);
     }
+    return (0);
+}//FIM DE DECL_VAR
 
-}
-
-/** Verifica a estrutura de declaração de uma ou mais variaveis */
-void p_comando(FILE *inArquivo, TOKEN *token)
+/** COMANDO */
+int p_comando(FILE *inArquivo, TOKEN *token)
 {
-    while(TRUE)
+    if((*token).classificacao == CLASSE_IDENTIFICADOR || (*token).classificacao == CLASSE_ABRI_CHAVE)
     {
-        if((*token).classificacao == CLASSE_IDENTIFICADOR || (*token).classificacao == CLASSE_ABRI_CHAVE)
-        {
-            p_comandoBasico(inArquivo, token);
-        }
-        else if((*token).classificacao == CLASSE_KW_DO || (*token).classificacao == CLASSE_KW_WHILE)
-        {
-            p_iteracao(inArquivo, token);
-        }
-        else if((*token).classificacao == CLASSE_KW_IF)
-        {
-            printf("\nchama p_if\n");
-            break;
-        }
-        else
-        {
-            break;
-        }
+        p_comandoBasico(inArquivo, token);
     }
-}
+    else if((*token).classificacao == CLASSE_KW_DO || (*token).classificacao == CLASSE_KW_WHILE)
+    {
+        p_iteracao(inArquivo, token);
+    }
+    else if((*token).classificacao == CLASSE_KW_IF)
+    {
+        p_ifElse(inArquivo, token);
+    }
+    else{
+        return (0);
+    }
+    return (1);
+}//FIM DE COMANDO
 
+/** COMANDO BASICO */
 void p_comandoBasico(FILE *inArquivo, TOKEN *token)
 {
     if((*token).classificacao == CLASSE_IDENTIFICADOR)
@@ -156,8 +157,9 @@ void p_comandoBasico(FILE *inArquivo, TOKEN *token)
     {
         p_bloco(inArquivo, token);
     }
-}
+}//FIM DE COMANDO BASICO
 
+/** ATRIBUIÇÃO */
 void p_atribuicao(FILE* inArquivo, TOKEN *token)
 {
     if((*token).classificacao == CLASSE_IDENTIFICADOR)
@@ -171,22 +173,20 @@ void p_atribuicao(FILE* inArquivo, TOKEN *token)
             {
                 p_chamaScanner(inArquivo, token);
             }
-            else
-            {
+            else{
                 p_erroMsg((*token), "o token esperado era ;");
             }
         }
-        else
-        {
+        else{
             p_erroMsg((*token), "o token esperado era =");
         }
     }
-    else
-    {
+    else{
         p_erroMsg((*token), "o token esperado era um identificador");
     }
-}
+}//FIM DE ATRIBUIÇÃO
 
+/** EXPR_ARIT */
 void p_exprArit(FILE *inArquivo, TOKEN *token)
 {
     while((*token).classificacao == CLASSE_IDENTIFICADOR || (*token).classificacao == CLASSE_T_CHAR ||
@@ -198,13 +198,13 @@ void p_exprArit(FILE *inArquivo, TOKEN *token)
             p_chamaScanner(inArquivo, token);
             p_exprArit(inArquivo, token);
         }
-        else
-        {
+        else{
             break;
         }
     }
-}
+}//FIM DE EXPR_ARIT
 
+/** TERMO */
 void p_termo(FILE *inArquivo, TOKEN *token)
 {
     while((*token).classificacao == CLASSE_IDENTIFICADOR || (*token).classificacao == CLASSE_T_CHAR ||
@@ -216,14 +216,13 @@ void p_termo(FILE *inArquivo, TOKEN *token)
             p_chamaScanner(inArquivo, token);
             p_termo(inArquivo, token);
         }
-        else
-        {
+        else{
             break;
         }
     }
+}//FIM DE TERMO
 
-
-}
+/** FATOR */
 void p_fator(FILE *inArquivo, TOKEN *token)
 {
     if((*token).classificacao == CLASSE_ABRI_PARENTESE)
@@ -234,8 +233,7 @@ void p_fator(FILE *inArquivo, TOKEN *token)
         {
             p_chamaScanner(inArquivo, token);
         }
-        else
-        {
+        else{
             p_erroMsg((*token), "o token esperado era )");
         }
     }
@@ -244,12 +242,12 @@ void p_fator(FILE *inArquivo, TOKEN *token)
     {
         p_chamaScanner(inArquivo, token);
     }
-    else
-    {
+    else{
         p_erroMsg((*token), "o token esperado era um char ou float ou int ou identificador");
     }
-}
+}//FIM DE FATOR
 
+/** EXPR_RELAC */
 void p_exprRelac(FILE *inArquivo, TOKEN *token)
 {
     p_exprArit(inArquivo, token);
@@ -258,12 +256,12 @@ void p_exprRelac(FILE *inArquivo, TOKEN *token)
         p_chamaScanner(inArquivo, token);
         p_exprArit(inArquivo, token);
     }
-    else
-    {
+    else{
         p_erroMsg((*token), "o token esperado era um operador relacional == ou >= ou > ou < ou <= ou !=");
     }
-}
+}//FIM DE EXPR_RELAC
 
+/** ITERAÇÃO */
 void p_iteracao(FILE *inArquivo, TOKEN *token)
 {
     if((*token).classificacao == CLASSE_KW_WHILE)
@@ -276,22 +274,26 @@ void p_iteracao(FILE *inArquivo, TOKEN *token)
             if((*token).classificacao == CLASSE_FECHA_PARENTESE)
             {
                 p_chamaScanner(inArquivo, token);
-                p_comando(inArquivo, token);
+                if(!p_comando(inArquivo, token))
+                {
+                    p_erroMsg((*token), "o while deve conter pelo menos um comando.");
+                }
             }
-            else
-            {
+            else{
                 p_erroMsg((*token), "o token esperado era um )");
             }
         }
-        else
-        {
+        else{
             p_erroMsg((*token), "o token esperado era um (");
         }
     }
     else if((*token).classificacao == CLASSE_KW_DO)
     {
         p_chamaScanner(inArquivo, token);
-        p_comando(inArquivo, token);
+        if(!p_comando(inArquivo, token))
+        {
+            p_erroMsg((*token), "o do-while deve conter pelo menos um comando.");
+        }
         if((*token).classificacao == CLASSE_KW_WHILE)
         {
             p_chamaScanner(inArquivo, token);
@@ -306,36 +308,67 @@ void p_iteracao(FILE *inArquivo, TOKEN *token)
                     {
                         p_chamaScanner(inArquivo, token);
                     }
-                    else
-                    {
-                        p_erroMsg((*token), "o token esperado era um ;)");
+                    else{
+                        p_erroMsg((*token), "o token esperado era um ;");
                     }
                 }
-                else
-                {
+                else{
                     p_erroMsg((*token), "o token esperado era um )");
                 }
             }
-            else
-            {
+            else{
                 p_erroMsg((*token), "o token esperado era um (");
             }
         }
-        else
-        {
+        else{
             p_erroMsg((*token), "o token esperado era a palavra chave while");
         }
     }
-    else
-    {
+    else{
         p_erroMsg((*token), "o token esperado era a palavra chave do ou while");
     }
-}
+}//FIM DE ITEREÇÃO
+
+/** IF_ELSE */
+void p_ifElse(FILE *inArquivo, TOKEN *token)
+{
+    if((*token).classificacao == CLASSE_KW_IF)
+    {
+        p_chamaScanner(inArquivo, token);
+        if((*token).classificacao == CLASSE_ABRI_PARENTESE)
+        {
+            p_chamaScanner(inArquivo, token);
+            p_exprRelac(inArquivo, token);
+            if((*token).classificacao == CLASSE_FECHA_PARENTESE)
+            {
+                p_chamaScanner(inArquivo, token);
+                if(!p_comando(inArquivo, token))
+                {
+                    p_erroMsg((*token), "o if deve conter pelo menos um comando.");
+                }
+                if((*token).classificacao == CLASSE_KW_ELSE)
+                {
+                    p_chamaScanner(inArquivo, token);
+                    if(!p_comando(inArquivo, token))
+                    {
+                        p_erroMsg((*token), "o else deve conter pelo menos um comando.");
+                    }
+                }
+            }
+            else{
+                p_erroMsg((*token), "o token esperado era um )");
+            }
+        }
+        else{
+            p_erroMsg((*token), "o token esperado era um (");
+        }
+    }
+}//FIM DE IF_ELSE
 
 /****************************************************************************
 *Funções auxiliares do parser
 */
-
+//CHAMA SCANNER
 void p_chamaScanner(FILE *inArquivo, TOKEN *token)
 {
     int tmp=0;
@@ -349,6 +382,7 @@ void p_chamaScanner(FILE *inArquivo, TOKEN *token)
     }
 }
 
+//CABEÇALHO PADRÃO DE ERRO
 void p_erroMsg(TOKEN token, char *msg)
 {
     printf("ERRO na linha %i, coluna %i, ultimo token lido %s: %s \n",
--- parser.h
+++ parser.h
@@ -1,26 +1,37 @@
+/**
+* Aluno: Dimas Francisco dos Santos - Matricula: 201210796.8
+* Data: 30/9/2014
+***************************************************************************************************
+* Arquivo:  "parser.h"
+* Dependências: "auto_symbol_coment.c" "auto_cahr.c" "scanner.c" "auto_ident_pChave.c" "auto_int_float.c"
+*               "scanner.h" "parser.c"
+***************************************************************************************************
+* Descrição: Contém a todas as definições das funções do parser
+*/
+
 #ifndef PARSER_H_INCLUDED
 #define PARSER_H_INCLUDED
 
 #include "scanner.h"
 
-int parser(FILE *inArquivo);
+/** Parser */
+void parser(FILE *inArquivo);
 void p_programa(FILE *inArquivo, TOKEN *token);
 void p_bloco(FILE *inArquivo, TOKEN *token);
-void p_declVar(FILE *inArquivo, TOKEN *token);
+int  p_declVar(FILE *inArquivo, TOKEN *token);
 void p_atribuicao(FILE* inArquivo, TOKEN *token);
 void p_exprArit(FILE *inArquivo, TOKEN *token);
 void p_termo(FILE *inArquivo, TOKEN *token);
 void p_fator(FILE *inArquivo, TOKEN *token);
-/////ok testado
-
-void p_comando(FILE *inArquivo, TOKEN *token);
+void p_ifElse(FILE *inArquivo, TOKEN *token);
+int  p_comando(FILE *inArquivo, TOKEN *token);
 void p_comandoBasico(FILE *inArquivo, TOKEN *token);
 void p_exprRelac(FILE *inArquivo, TOKEN *token);
 void p_iteracao(FILE *inArquivo, TOKEN *token);
 
 
 
-
+/** Auxiliares */
 void p_chamaScanner(FILE *inArquivo, TOKEN *token);
 void p_erroMsg(TOKEN token, char *msg);
 
